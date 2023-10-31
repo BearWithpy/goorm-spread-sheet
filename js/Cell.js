650f242c02a969c008d01b2ebf80401949d1f405
Junsu Park <pksjmh5295@gmail.com>
2023-10-31 09:55:02 +0900

    Highlight selected cell

--- js/Cell.js
+++ js/Cell.js
@@ -1,10 +1,21 @@
 export class Cell {
-    constructor(isHeader, disabled, data, row, column, active = false) {
+    constructor(
+        isHeader,
+        disabled,
+        data,
+        row,
+        column,
+        rowName,
+        columnName,
+        active = false
+    ) {
         this.isHeader = isHeader
         this.disabled = disabled
         this.data = data
         this.row = row
         this.column = column
         this.active = active
+        this.rowName = rowName
+        this.columnName = columnName
     }
 }
--- js/script.js
+++ js/script.js
@@ -13,19 +13,37 @@ function initSpreadSheet() {
         let spreadSheetRow = []
         for (let j = 0; j < COLS; j++) {
             let cellData = ""
+            let isHeader = false
+            let disabled = false
 
             if (j === 0) {
                 cellData = i
+                isHeader = true
+                disabled = true
             }
 
             if (i === 0) {
                 cellData = uppercaseAlphabet[j - 1]
+                isHeader = true
+                disabled = true
             }
+
             if (!cellData) {
                 cellData = ""
             }
+            const rowName = i
+            const colName = uppercaseAlphabet[j - 1]
 
-            const cell = new Cell(false, false, cellData, i, j, false)
+            const cell = new Cell(
+                isHeader,
+                disabled,
+                cellData,
+                i,
+                j,
+                rowName,
+                colName,
+                false
+            )
             spreadSheetRow.push(cell)
         }
         spreadSheet.push(spreadSheetRow)
@@ -40,7 +58,45 @@ function createCellElement(cell) {
     cellElement.id = "cell_" + cell.row + cell.column
     cellElement.value = cell.data
     cellElement.disabled = cell.disabled
+
+    if (cell.isHeader) {
+        cellElement.classList.add("header")
+    }
+
+    cellElement.onclick = () => handleCellClick(cell)
+
     return cellElement
+}
+
+function clearHeaderState() {
+    const headers = document.querySelectorAll(".header")
+    headers.forEach((element) => {
+        element.classList.remove("active")
+    })
+}
+
+function getElemFromRowCol(row, col) {
+    // return document.querySelector("#cell_" + row + col)
+    return document.querySelector(`#cell_${row}${col}`)
+}
+
+function handleCellClick(cell) {
+    clearHeaderState()
+    const columnHeader = spreadSheet[0][cell.column]
+    const rowHeader = spreadSheet[cell.row][0]
+
+    const columnHeaderElement = getElemFromRowCol(
+        columnHeader.row,
+        columnHeader.column
+    )
+    const rowHeaderElement = getElemFromRowCol(rowHeader.row, rowHeader.column)
+
+    columnHeaderElement.classList.add("active")
+    rowHeaderElement.classList.add("active")
+
+    document.querySelector(
+        "#cell-status"
+    ).innerHTML = `${cell.columnName}${cell.rowName}`
 }
 
 function displaySheet() {
